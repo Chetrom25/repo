7c514d734098f5dc34dbfd028b79c54ca900fa3a
chetrom25 <roman.chetvertak@hengsoft.net>
2023-05-22 16:47:04 +0300

    host changed2

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const http = require("http");
-const host = 'localhost';
 const port = 8000;
 
 const requestListener = (req, res) => {
     res.writeHead(200);
-    res.end("My first222222222222 server!");
+    res.end("My first server!");
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
